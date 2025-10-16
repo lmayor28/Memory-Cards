18bb14667eca6068d42eb2643151c6115ecfa9e6
RocioMDonofrio <rocimeldonofrio@gmail.com>
2025-10-16 16:42:21 -0300

    Actualizacion de index.js y la agregacion de los archivos a View.

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import GameView from '../views/GameView.vue'
-import ScoreView from '../views/ScoreView.vue'
+import StatsView from '../views/StatsView.vue'
+import BestScoreView from '../views/BestScoreView.vue'
+import CardsView from '../views/CardsView.vue'
+import AddCardView from '../views/AddCardView.vue'
+import LoginView from '../views/LoginView.vue'
 import NotFoundView from '../views/NotFoundView.vue'
 
 const routes = [
   { path: '/', component: HomeView },
   { path: '/juego', component: GameView },
-  { path: '/puntajes', component: ScoreView },
+  { path: '/puntajes', component: StatsView },
+  { path: '/mejor-puntaje', component: BestScoreView },
+  { path: '/cartas', component: CardsView },
+  { path: '/agregar-carta', component: AddCardView },
+  { path: '/iniciar-sesion', component: LoginView },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView }
 ]
 
